5ca21026baff08f9b48b4ad6e21d6c86072ace29
muhsenalkhalil <muhsenalkhalil92@gmail.com>
2019-08-19 17:19:54 +0300

    adad

--- db/dev_seeds.rb
+++ db/dev_seeds.rb
@@ -1,4 +1,5 @@
-
+require "database_cleaner"
+DatabaseCleaner.clean_with :truncation
 @logger = Logger.new(STDOUT)
 @logger.formatter = proc do |_severity, _datetime, _progname, msg|
                       msg unless @avoid_log
